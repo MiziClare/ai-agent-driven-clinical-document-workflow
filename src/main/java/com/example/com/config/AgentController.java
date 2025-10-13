4d6e21591b6aec4b47f6f59700f7ce9621a7546b
Xiaoyi <Xiaoyi.Zhang21@student.xjtlu.edu.cn>
2025-10-13 17:23:09 -0400

    Add tools examples and their http tests

--- src/main/java/com/example/com/config/AgentController.java
+++ src/main/java/com/example/com/config/AgentController.java
@@ -2,7 +2,6 @@ package com.example.com.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,11 +14,10 @@ public class AgentController {
     private final ChatClient serviceChatClient;
 
     @RequestMapping(value = "/agent", produces = "text/html;charset=UTF-8")
-    public Flux<String> service(String prompt, String chatId) {
+    public Flux<String> service(String prompt) {
 
         return serviceChatClient.prompt()
                 .user(prompt)
-                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, chatId) )
                 .stream()
                 .content();
     }
